3558f72abaa9d3b6a45b8598b934e20b52fb13a3
jblagana <jan.rhey.lagana@eee.upd.edu.ph>
2024-01-05 04:13:19 +0800

    working code

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import opendssdirect as dss
 import pygad
+from tabulate import tabulate
+import sys
+import logging
+logging.basicConfig(level=logging.WARNING)
+
 
 # Load dss file
 dss.Text.Command('Redirect ieee13.dss')
@@ -17,24 +22,51 @@ def edit_cap_loc(new_buses):
         dss.Text.Command(f'Edit Capacitor.{cap} bus1={new_buses[i]}')
 
 def fitness_func(ga_instance, solution, solution_idx):
+    str_solution = [str_gene_space[int(gene)] for gene in solution]
     nominal_voltage = 1
-    node_voltages = simulate(solution)
+    node_voltages = simulate(str_solution)
     # Calculate the mean squared error
     mse = sum((v - nominal_voltage) ** 2 for v in node_voltages) / len(node_voltages)
     return mse
 
 
+def on_generation(ga_instance):
+    label = f"{ga_instance.generations_completed}/{ga_instance.num_generations}"
+    progress = ga_instance.generations_completed / ga_instance.num_generations
+    progress_bar_len = 50
+    filled_len = int(progress_bar_len * progress)
+    bar = '=' * filled_len + '-' * (progress_bar_len - filled_len)
+    sys.stdout.write(f'\rGeneration progress: [{bar}] {progress * 100:.1f}% [{label}]')
+    sys.stdout.flush()  # Ensure the output is displayed immediately
+
 # Define the GA parameters
+str_gene_space = dss.Circuit.AllNodeNames()
+gene_space = [list(range(len(str_gene_space)))]*2   # Map the strings to integers
+
 ga_instance = pygad.GA(
-    num_generations=10,
-    num_parents_mating=5,
+    num_generations=500,
+    num_parents_mating=25,
     fitness_func=fitness_func,
-    sol_per_pop=10,
+    sol_per_pop=50,
     num_genes=2,
-    gene_space=[dss.Circuit.AllNodeNames()]*2
+    gene_space=gene_space,
+    on_generation=on_generation,
+    mutation_num_genes=1
 )
 
+print('\n')
 ga_instance.run()
 solution, solution_fitness, solution_idx = ga_instance.best_solution()
-print("Best solution : ", solution)
-print("Best solution fitness : ", solution_fitness)
+str_solution = [str_gene_space[int(gene)] for gene in solution]
+
+cap_names = dss.Capacitors.AllNames()
+cap_locations = str_solution
+data = list(zip(cap_names, cap_locations))
+print('\n')
+
+table = tabulate(data, headers=['Capacitor', 'Optimal Location'], tablefmt='fancy_grid')
+print(table)
+
+
+
+
